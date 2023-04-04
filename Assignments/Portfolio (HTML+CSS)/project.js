6c7f9392f8f3b6cd66ea0d7e8b92a35861d18a31
shuklasp <shriprakashshukla8@gmail.com>
2023-04-04 19:44:00 +0530

    Assignment added

---
+++ Assignments/Portfolio (HTML+CSS)/project.js
@@ -0,0 +1,35 @@
+let projectData = [
+    {
+        image: 'img/project-1.png',
+        name: 'project one',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript, #fullstack, #css'
+    },
+]
+
+const createProjectCards = (data) => {
+    let projectContainer = document.querySelector('.project-container');
+
+    projectContainer.innerHTML += `
+            <div class="project-card" data-tags="${data.tags}">
+                <div class="project-wrapper">
+                    <div class="project-thumbnail">
+                        <img src="img/close.png" class="close-btn" alt="">
+                        <img src="${data.image}" class="project-img" alt="">
+                        <span class="tags">${data.tags}</span>
+                    </div>
+
+                    <div class="project-body">
+                        <h1 class="project-name">${data.name}</h1>
+                        <p class="project-detail">${data.detail}</p>
+                        <a href="${data.github}" class="btn">github</a>
+                        <a href="${data.live}" class="btn">see live</a>
+                    </div>
+                </div>
+            </div>
+    `;
+}
+
+projectData.forEach(data => createProjectCards(data));
